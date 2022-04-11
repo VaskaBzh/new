8bf1e0c63f03f2a9474f4edcd05c9d701d1b7ae1
Vasily <vasily-bazhenov.03@mail.ru>
2022-04-11 15:04:57 +0300

    refact: add mobile filter button & input values on cards page

--- js/script-bedroom.js
+++ js/script-bedroom.js
@@ -15,12 +15,44 @@ $(document).ready(function () {
     let filterClose = document.querySelector('.search__filters-back')
     let link = document.querySelectorAll('.search__col')
     let click = document.querySelectorAll('.click')
+    let buttonApply = document.querySelector('.search__block-apply')
+    let body = document.querySelector('body')
+    let handleFrom = document.querySelector('.from')
+    let handleTo = document.querySelector('.to')
+    let from = document.querySelector('.irs-from')
+    let to = document.querySelector('.irs-to')
+    let inputFrom = document.getElementById('searchFrom')
+    let inputTo = document.getElementById('searchTo')
+
+    const inputStart = function () {
+      inputTo.value = to.textContent
+      inputFrom.value = from.textContent
+    }
+
+    handleFrom.addEventListener('mousemove', () => {
+      inputFrom.value = from.textContent
+    })
+
+    handleTo.addEventListener('mousemove', () => {
+      inputTo.value = to.textContent
+    })
+    handleFrom.addEventListener('mouseout', () => {
+      inputFrom.value = from.textContent
+    })
+    handleTo.addEventListener('mouseout', () => {
+      inputTo.value = to.textContent
+    })
   
     filter.addEventListener('click', function () {
       containerFilter.classList.add('active__container')
+      body.classList.add('no-scroll')
+      buttonApply.classList.add('active__container')
     })
+
     filterClose.addEventListener('click', function () {
       containerFilter.classList.remove('active__container')
+      body.classList.remove('no-scroll')
+      buttonApply.classList.remove('active__container')
     })
 
     for( let i = 0; i < link.length; i++ ) {
@@ -62,4 +94,6 @@ $(document).ready(function () {
             prevEl: '.search-swiper-button-prev',
           },
         });
+
+      inputStart()
   });
